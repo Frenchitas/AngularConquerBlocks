f96ee7b8e340a8ff6de57cf18c49c6e6eab7dfbb
Frenchitas <134218245+Frenchitas@users.noreply.github.com>
2024-12-01 09:40:07 -0300

    auth

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,17 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable, map } from 'rxjs';
+import { AuthData, LoginResponse } from '../models/auth.model';
+import { AuthAdapter } from './adapters';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private readonly baseUrl = 'http://localhost:4000/auth';
+  http: HttpClient = inject(HttpClient);
 
-  constructor() { }
+  login(user: AuthData): Observable<string> {
+    return this.http.post<LoginResponse>(`${this.baseUrl}/login`, user).pipe(map(AuthAdapter));
+  }
 }
